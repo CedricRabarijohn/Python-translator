b2a98cddb03eb033628a2878a9daf233bbc0d441
Cedric <cedrictianarabarijohn@gmail.com>
2022-08-25 09:09:36 +0400

    Translator

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from translate import translate
+from pydantic import BaseModel
+class TranslationModel(BaseModel):
+    text: str = 'The text to translate'
+    to_language: str = 'fr'
+
+app = FastAPI()
+
+@app.post("/translate")
+async def root(translationBody: TranslationModel):
+    fromLang = "auto"
+    text = translationBody.text
+    toLang = translationBody.to_language
+    res = {
+        "from_language": fromLang,
+        "to_language": toLang,
+        # "translated":"Ceci est la traduction"
+        "translated": translate(text, from_language=fromLang, to_language=toLang)
+    }
+    return res
