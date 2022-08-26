51e5bed1b921067a85bfd494fec8d05f42fd4d03
Cedric <cedrictianarabarijohn@gmail.com>
2022-08-26 11:01:25 +0400

    Testing older algo

--- module/controllers/translation/translation_controller.py
+++ module/controllers/translation/translation_controller.py
@@ -1,10 +1,8 @@
-from cgitb import text
 import imp
 from module.services.translation.translation_service import translate_from_google
 from module.models.translation.TranslationModelV1 import TranslationModelV1
 from module.models.translation.TranslationModelV2 import TranslationModelV2
 from module.services.utils import utils
-
 # V2
 async def translate_v2(translation_body_v2: TranslationModelV2):
     # The base language (ex: en, fr, es)
@@ -13,10 +11,20 @@ async def translate_v2(translation_body_v2: TranslationModelV2):
     texts = translation_body_v2.texts
     # The language to translate the texts to (ex: en, fr, es)
     to_lang = translation_body_v2.to_language
-    # Translate each field
-    i = 0
+    # An array that will contain the values of the texts
+    text_arr = []
     for attr in texts:
-        texts[attr] = translate_from_google(texts[attr], from_language=from_lang, to_language=to_lang)
+        text_arr.append(texts[attr])
+    # Contains the texts combined in one text separated with a separator
+    text_concatened = '|||'.join(text_arr)
+    # Translate the text
+    translated_text = translate_from_google(text_concatened, from_language=from_lang, to_language=to_lang)
+    # An array or the translated texts
+    translated_arr = translated_text.split(' ||| ')
+    i = 0
+    # Change the fields values to become the translated ones
+    for attr in texts:
+        texts[attr] = translated_arr[i]
         i += 1
     # Payload of the response
     res = {
