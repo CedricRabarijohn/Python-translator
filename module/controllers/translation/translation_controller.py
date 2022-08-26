f23d20c8b2547bc7d5220c55ab8a4f65f0187fb2
Cedric <cedrictianarabarijohn@gmail.com>
2022-08-26 10:49:40 +0400

    Fix algo

--- module/controllers/translation/translation_controller.py
+++ module/controllers/translation/translation_controller.py
@@ -1,3 +1,4 @@
+from cgitb import text
 import imp
 from module.services.translation.translation_service import translate_from_google
 from module.models.translation.TranslationModelV1 import TranslationModelV1
@@ -12,20 +13,10 @@ async def translate_v2(translation_body_v2: TranslationModelV2):
     texts = translation_body_v2.texts
     # The language to translate the texts to (ex: en, fr, es)
     to_lang = translation_body_v2.to_language
-    # An array that will contain the values of the texts
-    text_arr = []
-    for attr in texts:
-        text_arr.append(texts[attr])
-    # Contains the texts combined in one text separated with a separator
-    text_concatened = '|||'.join(text_arr)
-    # Translate the text
-    translated_text = translate_from_google(text_concatened, from_language=from_lang, to_language=to_lang)
-    # An array or the translated texts
-    translated_arr = translated_text.split(' ||| ')
+    # translate
     i = 0
-    # Change the fields values to become the translated ones
     for attr in texts:
-        texts[attr] = translated_arr[i]
+        texts[attr] = translate_from_google(texts[attr], from_language=from_lang, to_language=to_lang)
         i += 1
     # Payload of the response
     res = {
